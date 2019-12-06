44d112a85447c6df408f8ea07de9c234f7cda1b1
Dennis Wei <dw2654@columbia.edu>
2019-12-06 01:26:44 -0800

    added timing for day 6

--- 6/solution.py
+++ 6/solution.py
@@ -1,5 +1,6 @@
-from collections import defaultdict
 import networkx as nx
+
+import time
 
 def split_orbit(orbit):
     return orbit.strip().split(')')
@@ -20,27 +21,7 @@ with open("input.txt", 'r') as f:
 with open("sample_input.txt", 'r') as f:
     sample_orbits = [split_orbit(o) for o in f]
 
-def main(input):
-    orbits = {}
-    for o in input:
-        orbits[o[1]] = o[0]
-
-    memoized_orbits = defaultdict(int)
-
-    sum = 0
-    for o in orbits:
-        sum += get_num_orbits(o, orbits, memoized_orbits)
-    print(orbits)
-    print(memoized_orbits)
-    print(sum)
-
-# main(sample_orbits)
-# main(orbits)
-
-with open("sample_input2.txt", 'r') as f:
-    sample_orbits2 = [split_orbit(o) for o in f]
-
-def main2(input):
+def setup_graph(input):
     G = nx.Graph()
     orbits = {}
     for o in input:
@@ -51,8 +32,40 @@ def main2(input):
     for o1, o2 in orbits.items():
         G.add_edge(o1, o2)
         G.add_edge(o2, o1)
-    
-    print(len(nx.shortest_path(G,source="SAN", target="YOU")) - 3)
+    return G
 
-main2(sample_orbits2)
+def main(input):
+    orbits = {}
+    for o in input:
+        orbits[o[1]] = o[0]
+
+    memoized_orbits = {}
+
+    s = 0
+    for o in orbits:
+        s += get_num_orbits(o, orbits, memoized_orbits)
+    print("Solution for Part 1: ", s)
+
+    # G = setup_graph(input)
+
+    # res = sum(nx.shortest_path_length(G, source=n, target="COM") for n in G.nodes())
+    # print("Solution for Part 1: ", res)
+
+# main(sample_orbits)
+start = time.time()
+main(orbits)
+print(f'Part 1 took {time.time() - start} seconds')
+
+with open("sample_input2.txt", 'r') as f:
+    sample_orbits2 = [split_orbit(o) for o in f]
+
+def main2(input):
+    G = setup_graph(input)
+    
+    res = len(nx.shortest_path(G,source="SAN", target="YOU")) - 3
+    print("Solution for Part 2: ", res)
+
+# main2(sample_orbits2)
+start = time.time()
 main2(orbits)
+print(f'Part 2 took {time.time() - start} seconds')
